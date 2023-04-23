ecd3529cb2954506e6137f18e944df16ae0a95a1
SergeantMeatWad <104181364+SergeantMeatWad@users.noreply.github.com>
2023-04-23 18:03:43 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId:         'us-east-2_FZSAZgK2q',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '55r5omtm3vgsaoirfhd2s3054p',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId:         'us-east-2_pOzyivcOt',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '997n2f8rj6haflfaclg0q14f7',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
